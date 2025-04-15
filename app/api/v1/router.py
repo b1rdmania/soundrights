6610c7acb7de079bd2ac5a36b8378b847d42098a
b1rdmania <andy@cherrygalore.com>
2025-04-15 15:19:33 +0100

    Temporarily comment out Spotify router for debugging

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -4,7 +4,7 @@ from .endpoints import spotify, music, recognize
 api_router = APIRouter()
 
 # Include Spotify router with auth endpoints
-api_router.include_router(spotify.router, prefix="/spotify", tags=["spotify"])
+# api_router.include_router(spotify.router, prefix="/spotify", tags=["spotify"])
 
 # Include music router
 api_router.include_router(music.router, prefix="/music", tags=["music"])
